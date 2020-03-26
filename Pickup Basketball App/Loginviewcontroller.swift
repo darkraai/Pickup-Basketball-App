7013aefc21131b997fe7101da94683a68722c355
bsvo <bensvoboda@gmail.com>
2020-03-25 22:18:05 -0400

    Now when return is hit on keyboard, the keyboard goes away. I also saved text field values as variables

--- Pickup Basketball App/Loginviewcontroller.swift
+++ Pickup Basketball App/Loginviewcontroller.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class Loginviewcontroller: UIViewController {
 
-    
+    @IBOutlet weak var UsernameTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+
     @IBAction func Loginbuttonpressed(_ sender: Any) {
         print("button pressed")
         self.performSegue(withIdentifier: "Logintohomesegue", sender: self)
@@ -20,8 +22,22 @@ class Loginviewcontroller: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Loaded sucessfully")
+        UsernameTextField.delegate = self
+        PasswordTextField.delegate = self
+
     }
 
-
+    @IBAction func LoginTapped(_ sender: Any) {
+        var user = UsernameTextField.text!
+        var pass = PasswordTextField.text!
+        print(user)
+        print(pass)
+    }
+    
 }
-
+extension Loginviewcontroller : UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
