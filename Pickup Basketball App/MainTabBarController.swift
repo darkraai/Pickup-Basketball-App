bf7e65a0d74a1ccece1fed05fc11578da62025ae
bsvo <bensvoboda@gmail.com>
2020-04-05 23:01:15 -0400

    Tab bar controller functioning and the first tab set to home with icon

---
+++ Pickup Basketball App/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  Pickup Basketball App
+//
+//  Created by Ben Svoboda on 4/5/20.
+//  Copyright © 2020 Hoop Break. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MainTabBarController: UITabBarController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
