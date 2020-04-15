0ff02b8d8d51fd4164c62750292271d1152ddaa8
bsvo <bensvoboda@gmail.com>
2020-04-14 23:18:13 -0400

    I started the baller profile page and had to import hella new images for lines and stuff. Still need to pass object to it and retrieve objects values

---
+++ Pickup Basketball App/BallerProfile.swift
@@ -0,0 +1,21 @@
+//
+//  BallerProfile.swift
+//  Pickup Basketball App
+//
+//  Created by Ben Svoboda on 4/14/20.
+//  Copyright © 2020 Hoop Break. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+
+class BallerProfile: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+}
