4ac59a9c3ba614ce850dfc34a125d4ee58d7e144
darkraai <44532045+darkraai@users.noreply.github.com>
2020-04-17 15:58:38 -0400

    finished create_game screen

--- Pickup Basketball App/Creategameviewcontroller.swift
+++ Pickup Basketball App/Creategameviewcontroller.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class Creategameviewcontroller: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class Creategameviewcontroller: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
 
-    @IBOutlet weak var startTimePicker: UIDatePicker!
-    @IBOutlet weak var endTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var endTimeTextField: UITextField!
     @IBOutlet weak var gameModePicker: UIPickerView!
     @IBOutlet weak var publicToggle: UISwitch!
     @IBOutlet weak var bringBallToggle: UISwitch!
@@ -22,6 +23,9 @@ class Creategameviewcontroller: UIViewController, UIPickerViewDataSource, UIPick
     var selectedGameMode = ""
     var selectedStartTime = ""
     var selectedEndTime = ""
+    var bringBall = true
+    var publicValue = true
+    
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         1
@@ -41,25 +45,100 @@ class Creategameviewcontroller: UIViewController, UIPickerViewDataSource, UIPick
 
     }
     
+    var activeTextField: UITextField!
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+         activeTextField = textField
+    }
+    
+    let datePicker = UIDatePicker()
+    
+    private func createDatePicker(forField field : UITextField){
+        
+        datePicker.datePickerMode = .time
+        datePicker.minuteInterval = 30
+        field.textAlignment = .center
+        
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //done bar button on toolbar
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done
+            , target: nil, action: #selector(donePressed))
+        
+        toolbar.setItems([doneBtn], animated: true)
+        
+        //assign toolbar
+        field.inputAccessoryView = toolbar
+        
+        //assign date picker to text field
+        field.inputView = datePicker
+        
+    }
+    
+    var startTimeTextFieldDate : Date?
+    var endTimeTextFieldDate : Date?
+    
+    @objc private func donePressed(){
+        //formatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        
+        if (activeTextField == startTimeTextField){
+            startTimeTextField.text = formatter.string(from: datePicker.date)
+            startTimeTextFieldDate = datePicker.date
+            updateDoneButtonState()
+        } else if (activeTextField == endTimeTextField){
+            endTimeTextField.text = formatter.string(from: datePicker.date)
+            endTimeTextFieldDate = datePicker.date
+            updateDoneButtonState()
+        }
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         gameModePicker.delegate = self
         gameModePicker.dataSource = self
+        startTimeTextField.delegate = self
+        endTimeTextField.delegate = self
         
         updateDoneButtonState()
+        self.createDatePicker(forField: startTimeTextField)
+        self.createDatePicker(forField: endTimeTextField)
     }
     
     private func updateDoneButtonState(){
         startHoopingButton.isEnabled = false
-        if (selectedGameMode != ""){
-            startHoopingButton.isEnabled = true
+        if (selectedGameMode != "" && endTimeTextFieldDate != nil && startTimeTextFieldDate != nil){
+            if (endTimeTextFieldDate!.compare(startTimeTextFieldDate!) == ComparisonResult.orderedDescending){
+                startHoopingButton.isEnabled = true
+            }
         }
         
     }
     
-
-
+    @IBAction func publicSwitchToggled(_ sender: UISwitch) {
+        if (sender.isOn){
+            publicValue = true
+        }else{
+            publicValue = false
+        }
+    }
+    
+    @IBAction func bringBallSwitchToggled(_ sender: UISwitch) {
+        if (sender.isOn){
+            bringBall = true
+        }
+        else{
+            bringBall = false
+        }
+    }
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
