d586c39b69e1bea6142ae2de999495be229aec4d
bsvo <bensvoboda@gmail.com>
2020-04-28 19:03:26 -0400

    Revert "Revert "implemented newcourtviewcontroller screen""
    This reverts commit a7414bbf66d3254d05faaad64a832e8bf2e178ca.

--- Pickup Basketball App/Newcourtviewcontroller.swift
+++ Pickup Basketball App/Newcourtviewcontroller.swift
@@ -10,24 +10,81 @@ import UIKit
 import MapKit
  
  
-class Newcourtviewcontroller: UIViewController {
+class Newcourtviewcontroller: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var parkNameTextField: UITextField!
+    @IBOutlet weak var numCourtsTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var indoorSwitch: UISwitch!
+    @IBOutlet weak var membershipSwitch: UISwitch!
+    @IBOutlet weak var startHoopingButton: UIButton!
+    
+    
+    var parkName = ""
+    var numCourts = ""
+    var address = ""
+    var indoorSelected = false
+    var membershipSelected = false
+    
  
-    var locCoord: CLLocationCoordinate2D?
+    var annotation: MKPointAnnotation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        parkNameTextField.delegate = self
+        numCourtsTextField.delegate = self
+        addressTextField.delegate = self
+        indoorSwitch.isOn = false
+        membershipSwitch.isOn = false
+        updateDoneButtonState()
     }
     
- 
-    /*
-    // MARK: - Navigation
- 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateDoneButtonState(){
+        startHoopingButton.isEnabled = false
+        if (parkName != "" && numCourts != ""){
+            startHoopingButton.isEnabled = true
+        }
     }
-    */
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == parkNameTextField{
+            parkName = parkNameTextField.text!
+        } else if textField == numCourtsTextField{
+            numCourts = numCourtsTextField.text!
+        } else {
+            address = addressTextField.text!
+        }
+        updateDoneButtonState()
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           // Hide the keyboard.
+           textField.resignFirstResponder()
+           return true
+       }
+    
+    @IBAction func indoorSwitchToggled(_ sender: UISwitch) {
+        if (sender.isOn){
+            indoorSelected = true
+        } else {
+            indoorSelected = false
+        }
+    }
+    
+    @IBAction func membershipSwitchToggled(_ sender: UISwitch) {
+        if (sender.isOn){
+            membershipSelected = true
+        } else {
+            membershipSelected = false
+        }
+    }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
  
 }
 
